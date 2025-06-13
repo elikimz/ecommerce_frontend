04b0d1b413d83d0f07dd48a2042a04757944b6f2
Elijah Kimani <elijahkimani1293@gmail.com>
2025-06-13 12:11:50 +0300

    Remove .env from git tracking

--- src/pages/profilepage.tsx
+++ src/pages/profilepage.tsx
@@ -383,7 +383,7 @@ const ProfilePage = () => {
             <input
               type={type}
               name={name}
-              value={(formData as unknown)[name]}
+              value={(formData as any)[name]}
               onChange={handleChange}
               className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-500"
             />
