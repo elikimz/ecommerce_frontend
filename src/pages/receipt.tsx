c9390dea1efbe51e8d79be59773df4aa6e71f6ee
Elijah Kimani <elijahkimani1293@gmail.com>
2025-06-21 22:25:09 +0300

    Remove .env from git tracking

--- src/pages/receipt.tsx
+++ src/pages/receipt.tsx
@@ -14,6 +14,7 @@ const ReceiptPage: React.FC = () => {
     isLoading,
     isError,
   } = useGetOrderByIdQuery(orderId ?? "");
+  
 
   const formatMoney = (num: number) =>
     `KES ${num.toLocaleString(undefined, { minimumFractionDigits: 2 })}`;
